ef87f3962db41709528203bcdfa2ed2bf3d61dbb
Pratyush Shukla <shukla1024@gmail.com>
2020-01-04 19:17:36 +0530

    dark detect

--- image segment.py
+++ image segment.py
@@ -3,69 +3,31 @@ from PIL import Image
 from PIL import ImageOps
 import numpy as np  
 import PIL
-filename = "raw_image_1.JPG"
+#filename = "raw_image_1.JPG"
 
-filename = "raw_3.jpeg"
+filename1 = "image00.jpg"
+filename2 = "image01.jpg"
+filename3 = "image02.jpg"
+filename4 = "image03.jpg"
 
-with Image.open(filename) as image: 
+with Image.open(filename1) as image: 
     width, height = image.size 
     
     
     
-    
-img = Image.open(filename).convert('L')
-
-#RESIZE
+image = Image.open(filename1).convert('L')
 basewidth = 300
-wpercent = (basewidth / float(img.size[0]))
-hsize = int((float(img.size[1]) * float(wpercent)))
-img = img.resize((basewidth, hsize), PIL.Image.ANTIALIAS)
-    
+wpercent = (basewidth / float(image.size[0]))
 
-#image to numpy array
-c=np.array(img)
+hsize = int((float(image.size[1]) * float(wpercent)))
 
-d=np.array(img)
-img.size
+img = image.resize((basewidth, hsize))
 
-d=c*0
+img_array=np.array(img)
 
-a = Image.fromarray(d)
+mean_1=np.mean(sum(img_array))
 
-a1 = Image.fromarray(c)
-
-
-
-i=0
-
-border=(0,0,0,100)
-x=ImageOps.crop(img,border)
-c=np.array(x)
-x.size
-d=x*0
-
-
-print(x)
-
-d=c.tolist()
-
-e=np.array(d)
-arr2im
-
-for k in range(3):
-    for i in range( len(c)):
-        for j in range(len(c)):
-            d[i][j][k]=255-c[i][j][k]
-
-#####
-############################################################
-
-gray = cv2.imread('filename', 0)
-plt.figure(x)
-
-plt.imshow(c[:][:][:], cmap='gray')
-
-plt.show()
+print(mean_1)
 
 
 
